fb128d3631cc28a5f3ea046aa77ba44c3ffcc3c2
Jarl-kelven <110428249+Jarl-kelven@users.noreply.github.com>
2023-09-17 06:54:49 +0100

    Added files via upload
    addition of new files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: '#C80040',
+        primaryLight: '#ff85ab'
+      }
+    },
+  },
+  plugins: [],
+}
+
